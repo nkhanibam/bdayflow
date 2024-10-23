b9e116e4dcf9e1bf493885b13afb408742086da8
nkhanibam <153957580+nkhanibam@users.noreply.github.com>
2024-10-23 16:24:41 -0400

    Update calculate_bdayflow.js

--- netlify/functions/calculate_bdayflow.js
+++ netlify/functions/calculate_bdayflow.js
@@ -5,9 +5,9 @@ exports.handler = async (event, context) => {
 
     if (1 <= todaysdate && todaysdate <= 15) {
         bdayflow = 1;
-    } else if (16 <= todaysdate && todaysdate <= 26) {
+    } else if (16 <= todaysdate && todaysdate <= 25) {
         bdayflow = 2;
-    } else if (todaysdate >= 27) {
+    } else if (todaysdate >= 26) {
         bdayflow = 3;
     } else {
         bdayflow = 0;  // Default value
